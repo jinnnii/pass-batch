83f16d72317da346682860fc4959d208089eea6a
jinnnii <jinnnii@naver.com>
2024-01-23 15:46:36 +0900

    add spring project

---
+++ src/main/java/com/jinnnii/pass/PassBatchApplication.java
@@ -0,0 +1,13 @@
+package com.jinnnii.pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassBatchApplication.class, args);
+	}
+
+}
