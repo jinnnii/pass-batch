c29c1f1622a52a6a82928ee4520b80bf9cfcb1e1
jinnnii <jinnnii@naver.com>
2024-01-26 14:11:31 +0900

    #6 - add repository
    기본 Repository 생성

---
+++ src/main/java/com/jinnnii/pass/repository/PassRepository.java
@@ -0,0 +1,7 @@
+package com.jinnnii.pass.repository;
+
+import com.jinnnii.pass.domain.PassEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PassRepository extends JpaRepository<PassEntity, String> {
+}
