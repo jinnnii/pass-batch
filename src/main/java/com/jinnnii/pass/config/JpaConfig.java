69a486e90d85c6677a6b673a8130839cb45e860c
jinnnii <jinnnii@naver.com>
2024-01-29 17:01:53 +0900

    #9 - Refactor : Imports - unused
    사용하지 않는 라이브러리 정리

--- src/main/java/com/jinnnii/pass/config/JpaConfig.java
+++ src/main/java/com/jinnnii/pass/config/JpaConfig.java
@@ -1,11 +1,8 @@
 package com.jinnnii.pass.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Optional;
 
 @EnableJpaAuditing
 @Configuration
