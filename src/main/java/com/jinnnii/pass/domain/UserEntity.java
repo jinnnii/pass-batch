9a67cbd8f465c307d71a4d8be1a34c63b8a06c12
jinnnii <55618713+jinnnii@users.noreply.github.com>
2024-02-02 22:44:42 +0900

    #14 - Refactor : Json String Type of Meta
    사용자 메타 정보에 카카오 UUID를 Json 형태로 삽입

--- src/main/java/com/jinnnii/pass/domain/UserEntity.java
+++ src/main/java/com/jinnnii/pass/domain/UserEntity.java
@@ -4,12 +4,17 @@ import com.jinnnii.pass.domain.constant.ActiveStatus;
 import com.jinnnii.pass.domain.constant.RoleType;
 import com.jinnnii.pass.domain.converter.ActiveStatusConverter;
 import com.jinnnii.pass.domain.converter.RoleTypeConverter;
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.Type;
+import org.springframework.util.StringUtils;
 
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 @Getter
@@ -33,8 +38,8 @@ public class UserEntity extends AuditingField{
     private String region;
     private Integer phone;
 
-    @Lob
-    private String meta;
+    @Setter @Lob @Type(JsonStringType.class)
+    private Map<String, Object> meta;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private Set<UserGroupEntity> userGroups = new LinkedHashSet<>();
@@ -43,7 +48,7 @@ public class UserEntity extends AuditingField{
         return new UserEntity(userId, password, role,status, null, null,null);
     }
 
-    private UserEntity(String userId, String password, RoleType role, ActiveStatus status, String region, Integer phone, String meta) {
+    private UserEntity(String userId, String password, RoleType role, ActiveStatus status, String region, Integer phone, Map<String, Object> meta) {
         this.userId = userId;
         this.password = password;
         this.role = role;
@@ -51,5 +56,10 @@ public class UserEntity extends AuditingField{
         this.region = region;
         this.phone = phone;
         this.meta = meta;
+    }
+
+    public String getUuid() {
+        if (meta.containsKey("uuid")) return String.valueOf(meta.get("uuid"));
+        return null;
     }
 }
